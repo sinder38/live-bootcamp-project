885e5601a892f7270edd82da938882ecdd205c77
sinder <smirnov.sinder@gmail.com>
2026-04-23 05:53:07 +0000

    Add minimal placeholders to complete the tests

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 
-use axum::{serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, serve::Serve, Router};
 use tokio::net::TcpListener;
+use tower_http::services::{ServeDir, ServeFile};
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -13,7 +14,16 @@ pub struct Application {
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        let router = Router::new();
+        let asset_dir =
+            ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+
+        let router = Router::new()
+            .route("/signup", get(signup))
+            .route("/login", get(login))
+            .route("/logout", get(logout))
+            .route("/verify-2fa", get(verify_2fa))
+            .route("/verify-token", get(verify_token))
+            .fallback_service(asset_dir);
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -31,4 +41,24 @@ impl Application {
     pub fn server(&self) -> &Serve<TcpListener, Router, Router> {
         &self.server
     }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK
 }
