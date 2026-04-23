ae40c39c78ef2660fa364aa86a54f9e932dc34d1
sinder <smirnov.sinder@gmail.com>
2026-04-23 05:50:01 +0000

    Add tests and endpoints for tdd

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,57 @@
+use auth_service::Application;
+
+macro_rules! helper {
+    ($name:ident, $path:expr) => {
+        pub async fn $name(&self) -> reqwest::Response {
+            self.http_client
+                .get(&format!("{}{}", &self.address, $path))
+                .send()
+                .await
+                .expect("Failed to execute request.")
+        }
+    };
+}
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build app");
+
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task
+        // to avoid blocking the main test thread.
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        let http_client = reqwest::Client::new(); // Create a Reqwest http client instance
+
+        // Create new `TestApp` instance and return it
+        TestApp {
+            address,
+            http_client,
+        }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    helper!(get_signup, "/signup");
+    helper!(get_login, "/login");
+    helper!(get_logout, "/logout");
+    helper!(get_verify_2fa, "/verify-2fa");
+    helper!(get_verify_token, "/verify-token");
+
+    // TODO: Implement helper functions for all other routes (signup, login, logout, verify-2fa, and verify-token)
+}
