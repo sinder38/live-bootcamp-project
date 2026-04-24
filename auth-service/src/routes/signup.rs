09651a70139287dbd1812a7d9b0be14d55e07baa
sinder <smirnov.sinder@gmail.com>
2026-04-24 21:43:03 +0000

    Implement signup endpoint

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,9 +1,6 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
-use serde::Deserialize;
-
-pub async fn signup(Json(_request): Json<SignupRequest>) -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
+use crate::{app_state::AppState, domain::User};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct SignupRequest {
@@ -11,4 +8,29 @@ pub struct SignupRequest {
     pub password: String,
     #[serde(rename = "requires2FA")]
     pub requires_2fa: bool,
+}
+
+#[derive(Serialize)]
+pub struct SignupResponse {
+    pub message: String,
+}
+
+pub async fn signup(
+    State(state): State<AppState>,
+    Json(request): Json<SignupRequest>,
+) -> impl IntoResponse {
+    let mut user_store = state.user_store.write().await;
+    let new_user = User {
+        email: request.email,
+        password: request.password,
+        requires_2fa: request.requires_2fa,
+    };
+
+    user_store.add_user(new_user).expect("Failed to add user");
+
+    let response = Json(SignupResponse {
+        message: "User created successfully!".to_string(),
+    });
+
+    (StatusCode::CREATED, response)
 }
