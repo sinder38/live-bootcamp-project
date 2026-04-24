bc967d398294125002b703329a2b3228c1f6dacd
sinder <smirnov.sinder@gmail.com>
2026-04-24 20:01:22 +0000

    Add domain and service layer scaffolding for auth service

---
+++ auth-service/src/domain/user.rs
@@ -0,0 +1,9 @@
+// The User struct should contain 3 fields. email, which is a String;
+// password, which is also a String; and requires_2fa, which is a boolean.
+pub struct User {
+    todo!()
+}
+
+impl User {
+    todo!() // add a constructor function called `new`
+}
